bebc33ec451f4837669abcb422089f5c49e9151e
unknown <SPUNIVO6@SPUNIVO6.sv.cds>
2018-12-21 07:27:37 -0600

    Proyecto de librería con LinQ y ViewModel

---
+++ LibreriaApp/LibreriaApp/Controllers/LibrosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using LibreriaApp.Models;
+
+namespace LibreriaApp.Controllers
+{
+    public class LibrosController : Controller
+    {
+        BaseContext con = new BaseContext();
+
+        public ActionResult Index()
+        {
+            //query normalita
+            //var query = from libro in con.Libros select libro;
+
+            var query = (from libro in con.Libros
+                            join autor in con.Autores on libro.Id_Autor equals autor.Id_Autor
+                            orderby autor.Id_Autor
+                            select new EscritosViewModel()
+                            {
+                               Autor = autor.Nombre + " " + autor.Apellido, 
+                               Libro = libro.Nombre,
+                               Paginas = libro.Paginas
+                            }).ToList();
+
+            return View(query);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.Id_Autor = new SelectList(con.Autores, "Id_autor", "Nombre");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LibrosModel data)
+        {
+            con.Libros.Add(data);
+            con.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LibreriaApp/LibreriaApp/Models/LibrosModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibreriaApp.Models
+{
+    public class LibrosModel
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int Id_Libro { get; set; }
+        public string Nombre { get; set; }
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+        [Display(Name = "Páginas")]
+        public int Paginas { get; set; }
+
+        public int Id_Autor { get; set; }
+        public virtual AutoresModel Autor { get; set; }
+    }
+
+
+}
